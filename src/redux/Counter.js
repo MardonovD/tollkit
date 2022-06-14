701e18ba936053c604f9307642ff3aa417e23f4f
Mardonov Dostonbek <mardonovdoston69@gmail.com>
2022-06-14 20:26:04 +0300

    increment

--- src/redux/Counter.js
+++ src/redux/Counter.js
@@ -1,2 +1,11 @@
 import { createSlice } from "@reduxjs/toolkit/dist/createSlice";
 
+export const counterSlice = counterSlice({
+  name: "counter",
+  initialState: { data: 0 },
+  reducers: {
+    increment: (state, action) => {
+      state.data += 1;
+    },
+  },
+});
